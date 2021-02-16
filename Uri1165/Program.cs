17a4ab13f167b6f2aae7d7a5f6f0851943252bc6
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:43:12 -0300

    Add files via upload

---
+++ Uri1165/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Uri1165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p, n;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                p = 0;
+
+                int x = int.Parse(Console.ReadLine());
+
+
+
+
+                for (int c = 1; c <= x; c++)
+                {
+                    if (x % c == 0.0)
+                    {
+                        p = p + 1;
+                       
+                    }
+                }
+
+                if (p == 2)
+                {
+                    Console.WriteLine(x + " É primo");
+                }
+                else
+                {
+                    Console.WriteLine(x + " Não é primo");
+                }
+
+            }
+        }
+    }
+}
